969338e05abf856caf2cae56a6d24fec319a7415
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2023-12-26 16:59:49 +0500

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Siddikjon Kuziboev on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
