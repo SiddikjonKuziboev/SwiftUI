47100f291ecaeb3ef3bc9e1444f46929ba5ce0f5
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-02-05 10:16:26 +0500

    scrumsview

---
+++ First/Views/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  First
+//
+//  Created by Siddikjon Kuziboev on 01/02/24.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.icon
+            configuration.title
+        }
+    }
+    
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
