2a1f739e8d7e513d3d1a43c19e03dcda393798b1
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-02-09 10:47:27 +0500

    binding and added theme picker

---
+++ First/Views/CustomView/ThemeView.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeView.swift
+//  First
+//
+//  Created by Siddikjon Kuziboev on 09/02/24.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundStyle(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+            
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
+
